4bdb9b082f9deb3ce0aab5d99dadc2a5ad253c8f
Taahira Carter <taahirac001@gmail.com>
2019-11-27 00:18:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,9 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+      programmer_hash[:grace_hopper] 
+      
 end
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +39,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +83,14 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[0][:yukihiro_matsumoto] = {
+ 		      :known_for => "Ruby",
+ 		      :languages => ["LISP", "C"]
+     }
+     
+     programmer_hash
+ 		    
     
 end
 
